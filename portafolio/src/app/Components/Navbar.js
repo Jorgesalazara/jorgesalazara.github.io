62ec2ddc400a4b696fc301dd5acf6de210b9d373
Jorge <jorgeernestosalazararrieta@gmail.com>
2024-11-07 13:24:00 -0500

    Navbar creada

---
+++ portafolio/src/app/Components/Navbar.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import styles from '../Styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src="/logo.png" alt="Logo" className={styles.logoImage} /> {/* Adjust logo path */}
+        <span>FAWZIUQ</span>
+      </div>
+      <ul className={styles.navLinks}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About Me</Link></li>
+        <li><Link href="/services">Services</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="/testimonials">Testimonials</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+        <li><a href="/resume.pdf" className={styles.downloadButton} download>Download CV</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
