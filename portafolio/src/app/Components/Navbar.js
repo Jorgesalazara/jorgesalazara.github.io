9cd893284fc5b54865bdfe8b4a01167dca23bb6e
Jorge <jorgeernestosalazararrieta@gmail.com>
2024-11-12 13:21:10 -0500

    Main routing done

--- portafolio/src/app/Components/Navbar.js
+++ portafolio/src/app/Components/Navbar.js
@@ -6,14 +6,13 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <img src="/logo.png" alt="Logo" className={styles.logoImage} /> {/* Adjust logo path */}
-        <span>FAWZIUQ</span>
+        <span>JORGE SALAZAR</span>
       </div>
       <ul className={styles.navLinks}>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/about">About Me</Link></li>
         <li><Link href="/services">Services</Link></li>
         <li><Link href="/projects">Projects</Link></li>
-        <li><Link href="/testimonials">Testimonials</Link></li>
         <li><Link href="/contact">Contact</Link></li>
         <li><a href="/resume.pdf" className={styles.downloadButton} download>Download CV</a></li>
       </ul>
